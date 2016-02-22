8a627c0829bb27bc192954c11e4b1539ca821e4a
Mateusz Wilk <mateusz.wilk@bbc.co.uk>
2016-02-22 17:28:39 +0000

    Init commit

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.all('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, function(){
+    console.log('Server listening on port: ' + PORT);
+});
+
